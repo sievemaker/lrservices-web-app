1c4e84b95c4505e4e4ddea69929ca6d7a8ffe7fa
lrzeszotarski <09quiz-papayas@icloud.com>
2024-02-26 21:43:47 +0100

    cleanup

--- src/components/LargeScreenAppBar/LargeScreenAppBar.tsx
+++ src/components/LargeScreenAppBar/LargeScreenAppBar.tsx
@@ -5,7 +5,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import BusinessIcon from "@mui/icons-material/Business";
-import {Button, Fade, Slide} from "@mui/material";
+import {Button, Container, Fade, MenuItem, Slide} from "@mui/material";
 import {Link as RouterLink} from "react-router-dom";
 import Typography from "@mui/material/Typography";
 
@@ -31,7 +31,30 @@ const LargeScreenAppBar: FC<LargeScreenAppBarProps> = () => {
     }, []);
 
     return (<Box sx={{flexGrow: 1}}>
-        <Box sx={{flexGrow: 1, minHeight: containerRef?.current?.scrollHeight.valueOf()}} ref={containerRef}>
+        <AppBar position="static" sx={{backgroundColor:'#000000'}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <img src={process.env.PUBLIC_URL + "/softinnotec.png"} width="20%"/>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        sx={{
+                            mr: 2,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Komplexität beherrschen, Qualität garantieren.
+                    </Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
+        <Box sx={{flexGrow: 1, minHeight: containerRef?.current?.scrollHeight.valueOf(), background: 'linear-gradient(to right bottom, #000000, #0B0B0B)'}} ref={containerRef}>
             <Slide in={isIn} timeout={{enter: 1000, exit: 100}}>
                 <img src={process.env.PUBLIC_URL + "/banner_large.png"} width="35%"/>
             </Slide>
--- src/components/SmallScreenAppBar/SmallScreenAppBar.tsx
+++ src/components/SmallScreenAppBar/SmallScreenAppBar.tsx
@@ -4,7 +4,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
 import BusinessIcon from "@mui/icons-material/Business";
-import {Button, Link, Menu, MenuItem, Slide} from "@mui/material";
+import {Button, Container, Link, Menu, MenuItem, Slide} from "@mui/material";
 import {Link as RouterLink} from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import MenuIcon from '@mui/icons-material/Menu';
@@ -25,6 +25,24 @@ const SmallScreenAppBar: FC<SmallScreenAppBarProps> = () => {
 
     return (
         <Box sx={{minHeight: containerRef?.current?.scrollHeight?.valueOf(), overflow: 'hidden'}} ref={containerRef}>
+            <AppBar position="static" sx={{backgroundColor:'#000000'}}>
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <img src={process.env.PUBLIC_URL + "/softinnotec.png"} width="20%"/>
+                        <Typography variant="body2" component="div" sx={{
+                            mr: 2,
+                            fontFamily: 'monospace',
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                            flexGrow: 1
+                        }}>
+
+                        Komplexität beherrschen, Qualität garantieren.
+                        </Typography>
+                    </Toolbar>
+                </Container>
+            </AppBar>
             <Slide in={isIn} timeout={{enter: 1000, exit: 100}}>
             <img src={process.env.PUBLIC_URL + "/banner_large.png"} width="80%"/>
 
